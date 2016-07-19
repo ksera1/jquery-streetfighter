84f703a52cc10f4910c80716c3a0c243249892c5
ksera1 <sarah.sareh@gmail.com>
2016-07-19 20:30:08 +0430

    added keydown and keyup methods

--- js/app.js
+++ js/app.js
@@ -1,26 +1,46 @@
 $(document).ready(function(){
-
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+	// play hadouken sound	
+	playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$(.'hadouken').show();
-	// play hadouken sound
-	// show hadouken and animate it to the right of the screen
-
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			});
+		//$(.'hadouken').show();
+	// animate it to the right of the screen
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ryu goes back to his ready position
-	});
+	})
+	.keydown(function(key) {
+		if (key.which == 88){
+		$('.ryu-still').hide();
+		$('.ryu-cool').show();
+	}
+	.keyup(function(key) {
+		if (key.which == 88){
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();		
+		}
+	}
 });
 
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
