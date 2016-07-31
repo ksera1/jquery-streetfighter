87ed4b4218983b74c21f8a29e45eb2180f9f0cdf
ksera1 <sarah.sareh@gmail.com>
2016-07-31 11:52:10 +0430

    .ryu div appears at the bottom of the page when mouse is inside .ryu div and x is pressed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     $('.ryu').mouseenter(function() {
             $('.ryu-still').hide();
             $('.ryu-ready').show();
@@ -9,12 +8,14 @@ $(document).ready(function() {
             $('.ryu-still').show();
         })
         .mousedown(function() {
-            // play hadouken sound	
+            // play hadouken sound  
+            //where did playHadouken() function come from?
             playHadouken();
             $('.ryu-ready').hide();
             $('.ryu-throwing').show();
             $('.hadouken').finish().show().animate({ 'left': '1020px' },
                 500,
+                //what does 'this' do?
                 function() {
                     $(this).hide();
                     $(this).css('left', '520px');
@@ -22,29 +23,12 @@ $(document).ready(function() {
             // $(.'hadouken').show();
             // animate it to the right of the screen
         })
-        
-        	// .mouseup(function() {
-        	// 	$('.ryu-throwing').hide();
-        	// 	$('.ryu-ready').show();
-        	// });
-        	// .keydown(function(key) {
-        	// 	if (key.which == 88){
-        	// 	$('.ryu-still').hide();
-        	// 	$('.ryu-cool').show();
-        	// }
-        	// .keyup(function(key) {
-        	// 	if (key.which == 88){
-        	// 	$('.ryu-cool').hide();
-        	// 	$('.ryu-still').show();		
-        	// 	}
-        	// }
-
-    .mouseup(function() {
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-    });
+        .mouseup(function() {
+            $('.ryu-throwing').hide();
+            $('.ryu-ready').show();
+        });
 });
-
+//keydown function isn't working
 $(document).keydown(function(e) {
     if (e.keyCode == 88) {
         $('.ryu-still').hide();
@@ -55,8 +39,16 @@ $(document).keydown(function(e) {
         $('.ryu-cool').hide();
         $('.ryu-still').show();
     }
+//when mouse is inside .ryu div and 'x' is
+// pressed addt'l div appears bottom of page.
+    // .off(){
+    //     if (e.keyCode == 88) {
+    //     $('.ryu-cool').show();
+    //     $('.ryu-still').hide();
+    //     }
+    //}
 });
-
+//why isn't this function inside of .mousedown listener?
 function playHadouken() {
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
