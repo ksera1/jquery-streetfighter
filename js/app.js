e21336bea4e6f0c8f89f546195d516de24724d62
ksera1 <sarah.sareh@gmail.com>
2016-07-31 11:05:33 +0430

    when mouse moves out of main and x is pressed images switches to ready position. fix this.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,64 @@
-$(document).ready(function(){
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-	// play hadouken sound	
-	playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show().animate(
-			{'left': '1020px'},
-			500,
-			function() {
-				$(this).hide();
-				$(this).css('left', '520px');
-			});
-		//$(.'hadouken').show();
-	// animate it to the right of the screen
-	})
-	.mouseup(function() {
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	})
-	.keydown(function(key) {
-		if (key.which == 88){
-		$('.ryu-still').hide();
-		$('.ryu-cool').show();
-	}
-	.keyup(function(key) {
-		if (key.which == 88){
-		$('.ryu-cool').hide();
-		$('.ryu-still').show();		
-		}
-	}
+$(document).ready(function() {
+
+    $('.ryu').mouseenter(function() {
+            $('.ryu-still').hide();
+            $('.ryu-ready').show();
+        })
+        .mouseleave(function() {
+            $('.ryu-ready').hide();
+            $('.ryu-still').show();
+        })
+        .mousedown(function() {
+            // play hadouken sound	
+            playHadouken();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').show();
+            $('.hadouken').finish().show().animate({ 'left': '1020px' },
+                500,
+                function() {
+                    $(this).hide();
+                    $(this).css('left', '520px');
+                });
+            // $(.'hadouken').show();
+            // animate it to the right of the screen
+        })
+        
+        	// .mouseup(function() {
+        	// 	$('.ryu-throwing').hide();
+        	// 	$('.ryu-ready').show();
+        	// });
+        	// .keydown(function(key) {
+        	// 	if (key.which == 88){
+        	// 	$('.ryu-still').hide();
+        	// 	$('.ryu-cool').show();
+        	// }
+        	// .keyup(function(key) {
+        	// 	if (key.which == 88){
+        	// 	$('.ryu-cool').hide();
+        	// 	$('.ryu-still').show();		
+        	// 	}
+        	// }
+
+    .mouseup(function() {
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 });
 
-function playHadouken () {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
+$(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+        $('.ryu-still').hide();
+        $('.ryu-cool').show();
+    }
+}).keyup(function(e) {
+    if (e.keyCode == 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+    }
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
 }
